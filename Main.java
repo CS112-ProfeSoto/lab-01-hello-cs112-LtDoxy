95c9050c3ea3d0af3f63f663a4f04e79e4b55461
LtDoxy <howard.luke131@gmail.com>
2025-06-14 17:17:38 -0700

    Finished parts 1 and 2

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+        Main.printMessage();
+        Main.printNumbers(37);
+    }
+
+    public static void printMessage() {
+        System.out.println("I like to code!");
+    }
+
+    public static int printNumbers (int number) {
+        System.out.println(number);
     }
 }
